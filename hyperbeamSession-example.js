e54387b8323812cf2abbab8338cd9453e71d0028
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2023-08-14 15:40:16 -0700

    add comment to hyperbeamSession-example.js

--- hyperbeamSession-example.js
+++ hyperbeamSession-example.js
@@ -1,3 +1,8 @@
+// Follow the instruction at
+// https://docs.hyperbeam.com/home/getting-started
+// and get the response with session_id, embed_url and admin_token.
+// Then paste it in the file hyperbeamSession.js
+
 export const hyperbeamSession = {
     "session_id":"aaaaaaa-bbbb-cccc-xxxx-zzzzzzz",
     "embed_url":"https://xxxxyyyy.hyperbeam.com/zzzzz-zzzzzz?token=mmmmmmmm",
